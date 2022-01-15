c65848bd58acf09e7b651dc3be6e20b43f1dc5c1
kantiman <krantitherevolution99@gmail.com>
2022-01-15 15:15:23 +0545

    asdasdsa

--- components/footer.js
+++ components/footer.js
@@ -1,67 +1,67 @@
 const Footer = () => {
     return ( 
         <>
-         <footer class="ftco-footer ftco-section">
-            <div class="container">
-                <div class="row mb-5">
-                <div class="col-md">
-                    <div class="ftco-footer-widget mb-4">
-                    <h2 class="ftco-heading-2">About Us</h2>
+         <footer className="ftco-footer ftco-section">
+            <div className="container">
+                <div className="row mb-5">
+                <div className="col-md">
+                    <div className="ftco-footer-widget mb-4">
+                    <h2 className="ftco-heading-2">About Us</h2>
                     <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-                    <ul class="ftco-footer-social list-unstyled float-md-left float-lft mt-5">
-                        <li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
-                        <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
-                        <li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
+                    <ul className="ftco-footer-social list-unstyled float-md-left float-lft mt-5">
+                        <li className="ftco-animate"><a href="#"><span className="icon-twitter"></span></a></li>
+                        <li className="ftco-animate"><a href="#"><span className="icon-facebook"></span></a></li>
+                        <li className="ftco-animate"><a href="#"><span className="icon-instagram"></span></a></li>
                     </ul>
                     </div>
                 </div>
-                <div class="col-md">
-                    <div class="ftco-footer-widget mb-4 ml-md-5">
-                    <h2 class="ftco-heading-2">Information</h2>
-                    <ul class="list-unstyled">
-                        <li><a href="#" class="py-2 d-block">Portfolio</a></li>
-                        <li><a href="#" class="py-2 d-block">Privacy</a></li>
-                        <li><a href="#" class="py-2 d-block">Terms Condition</a></li>
+                <div className="col-md">
+                    <div className="ftco-footer-widget mb-4 ml-md-5">
+                    <h2 className="ftco-heading-2">Information</h2>
+                    <ul className="list-unstyled">
+                        <li><a href="#" className="py-2 d-block">Portfolio</a></li>
+                        <li><a href="#" className="py-2 d-block">Privacy</a></li>
+                        <li><a href="#" className="py-2 d-block">Terms Condition</a></li>
                     </ul>
                     </div>
                 </div>
-                <div class="col-md">
-                    <div class="ftco-footer-widget mb-4">
-                    <h2 class="ftco-heading-2">Links</h2>
-                    <ul class="list-unstyled">
-                        <li><a href="#" class="py-2 d-block">Home</a></li>
-                        <li><a href="#" class="py-2 d-block">About</a></li>
-                        <li><a href="#" class="py-2 d-block">Work</a></li>
-                        <li><a href="#" class="py-2 d-block">Contact</a></li>
+                <div className="col-md">
+                    <div className="ftco-footer-widget mb-4">
+                    <h2 className="ftco-heading-2">Links</h2>
+                    <ul className="list-unstyled">
+                        <li><a href="#" className="py-2 d-block">Home</a></li>
+                        <li><a href="#" className="py-2 d-block">About</a></li>
+                        <li><a href="#" className="py-2 d-block">Work</a></li>
+                        <li><a href="#" className="py-2 d-block">Contact</a></li>
                     </ul>
                     </div>
                 </div>
-                <div class="col-md">
-                    <div class="ftco-footer-widget mb-4">
-                        <h2 class="ftco-heading-2">Have a Questions?</h2>
-                        <div class="block-23 mb-3">
+                <div className="col-md">
+                    <div className="ftco-footer-widget mb-4">
+                        <h2 className="ftco-heading-2">Have a Questions?</h2>
+                        <div className="block-23 mb-3">
                         <ul>
-                            <li><span class="icon icon-map-marker"></span><span class="text">203 Fake St. Mountain View, San Francisco, California, USA</span></li>
-                            <li><a href="#"><span class="icon icon-phone"></span><span class="text">+2 392 3929 210</span></a></li>
-                            <li><a href="#"><span class="icon icon-envelope"></span><span class="text">info@yourdomain.com</span></a></li>
+                            <li><span className="icon icon-map-marker"></span><span className="text">203 Fake St. Mountain View, San Francisco, California, USA</span></li>
+                            <li><a href="#"><span className="icon icon-phone"></span><span className="text">+2 392 3929 210</span></a></li>
+                            <li><a href="#"><span className="icon icon-envelope"></span><span className="text">info@yourdomain.com</span></a></li>
                         </ul>
                         </div>
                     </div>
                 </div>
                 </div>
-                <div class="row">
-                <div class="col-md-12 text-center">
+                <div className="row">
+                <div className="col-md-12 text-center">
 
-                <p>Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="icon-heart color-danger" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a></p>
+                <p>Copyright &copy;{document.write(new Date().getFullYear());} All rights reserved | This template is made with <i className="icon-heart color-danger" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a></p>
                 </div>
                 </div>
             </div>
             </footer>
             
-        <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
+        <div id="ftco-loader" className="show fullscreen"><svg className="circular" width="48px" height="48px"><circle className="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle className="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
 
 
-        <script src="js/jquery.min.js"></script>
+        {/* <script src="js/jquery.min.js"></script>
         <script src="js/jquery-migrate-3.0.1.min.js"></script>
         <script src="js/popper.min.js"></script>
         <script src="js/bootstrap.min.js"></script>
@@ -75,7 +75,7 @@ const Footer = () => {
         <script src="js/scrollax.min.js"></script>
         <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
         <script src="js/google-map.js"></script>
-        <script src="js/main.js"></script>
+        <script src="js/main.js"></script> */}
         </>
         
      );
