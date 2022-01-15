562d1adde4ed1ac43e590e2e5f146573807d8102
kantiman <krantitherevolution99@gmail.com>
2022-01-15 15:10:35 +0545

    first commit

---
+++ components/header.js
@@ -0,0 +1,34 @@
+import Head from 'next/head'
+const Header = () => {
+    return ( 
+        <Head>
+            <title>Evans - Free Bootstrap 4 Template by Colorlib</title>
+            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+            
+            <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900" rel="stylesheet" />
+            <link href="https://fonts.googleapis.com/css?family=Barlow+Condensed:900&display=swap" rel="stylesheet" />
+            <link href="https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i&display=swap" rel="stylesheet" />
+
+            <link rel="stylesheet" href="../assets/css/open-iconic-bootstrap.min.css" />
+            <link rel="stylesheet" href="../assets/css/animate.css" />
+            
+            <link rel="stylesheet" href="../assets/css/owl.carousel.min.css" />
+            <link rel="stylesheet" href="../assets/css/owl.theme.default.min.css" />
+            <link rel="stylesheet" href="../assets/css/magnific-popup.css" />
+
+            <link rel="stylesheet" href="../assets/css/aos.css" />
+
+            <link rel="stylesheet" href="../assets/css/ionicons.min.css" />
+
+            <link rel="stylesheet" href="../assets/css/bootstrap-datepicker.css" />
+            <link rel="stylesheet" href="../assets/css/jquery.timepicker.css" />
+
+            
+            <link rel="stylesheet" href="../assets/css/flaticon.css" />
+            <link rel="stylesheet" href="../assets/css/icomoon.css" />
+            <link rel="stylesheet" href="../assets/css/style.css"  rel="stylesheet" key="test"/>
+        </Head>
+     );
+}
+ 
+export default Header
